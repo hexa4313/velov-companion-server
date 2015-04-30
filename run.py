0eaeab8ba6d477b2a4174e793e2d56c22047fef9
Edouard Oger <edouard.oger@gmail.com>
2015-04-30 11:12:41 +0200

    use of docker compose

--- run.py
+++ run.py
@@ -1,7 +1,13 @@
+import os
 from flask import Flask
 from flask.ext import restful
+from flask.ext.sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres@' + os.environ['DB_PORT_5432_TCP_ADDR'] + '/velov-companion'
+db = SQLAlchemy(app)
+db.create_all()
+
 api = restful.Api(app)
 
 class User (restful.Resource):
