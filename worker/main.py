7e9c2d57a1d3a930fb9ca87acde85dd8ef7405f1
Edouard Oger <edouard.oger@gmail.com>
2015-05-03 16:47:53 +0200

    Merge schedulers jobs instead of using 2 schedulers

--- worker/main.py
+++ worker/main.py
@@ -1,20 +1,20 @@
 import os
+import logging
 
 from apscheduler.schedulers.blocking import BlockingScheduler
 from stations_fetcher import load_stations
 from tokens_deleter import delete_tokens
 
+logging.basicConfig()
 
 if __name__ == '__main__':
+
     scheduler = BlockingScheduler()
     scheduler.add_job(load_stations, 'interval', seconds=60)
+    scheduler.add_job(delete_tokens, 'interval', days=1)
     print('Press Ctrl+{0} to exit'.format('Break' if os.name == 'nt' else 'C'))
-
-    scheduler_24 = BlockingScheduler()
-    scheduler_24.add_job(delete_tokens, 'interval', days=1)
 
     try:
         scheduler.start()
-	scheduler_24.start()
     except (KeyboardInterrupt, SystemExit):
         pass
